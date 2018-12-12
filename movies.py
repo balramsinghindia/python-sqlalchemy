d91f6f4fb08e69538c689210ffb56d1bb70a5313
Balram Singh <singhbalram786@gmail.com>
2018-12-12 13:16:38 +0000

    Create movies.py

---
+++ movies.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from sqlalchemy import Column, String, Integer, Date
+
+from src.model.base import Base
+
+
+class   Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
